1871e50097e8a3d318a04f51b8e5b3f432abc823
Leonardo Ciocan <leonardo.ciocan@outlook.com>
2015-08-16 03:45:29 +0100

    Added workflow composed of tasks , runs on generated file stack.

--- core.js
+++ core.js
@@ -5,18 +5,56 @@ var from = getUserHome() + "/Downloads/";
 var fs = require("fs");
 var path = require("path");
 var tasks = require("tasks");
-var React = require('react');
+//var React = require('react');
+var dialog = require("remote").require("dialog");
 
 function getUserHome() {
   return process.env[(process.platform == 'win32') ? 'USERPROFILE' : 'HOME'];
 }
 
-var map = {
-  "get" : tasks.getFiles,
-  "rotate" : tasks.rotate
+var workflow = {
+    files : []
+};
+
+var commands = [
+  {
+      task : "copy",
+      options : {
+        context : workflow,
+        destination : getUserHome() + "/Downloads/f1/"
+      }
+  },
+  {
+      task: "rotate",
+      options : {
+        context : workflow
+      }
+  }
+];
+
+function execute(tasks , i){
+    if(i > tasks.length -1) return;
+    var task = tasks[i];
+    tasks[task.task](task.options , function(files){
+      workflow.files = files;
+      execute(tasks , i+1);
+    });
 }
 
+console.log("starting");
+tasks.getFiles(getUserHome()+"/Downloads/f0/" , "*.+(jpg|png)" , function(files){
+  workflow.files = files;
+  execute(commands , 0);
+});
+console.log("finishing");
+
 $(document).ready(function(){
+
+      $("#open").click(function(){
+        dialog = require("remote").require("dialog");
+        console.log(window);
+          dialog.showOpenDialog( window ,{ properties: [ 'openFile', 'openDirectory', 'multiSelections' ]});
+      });
 
         var holder = document.getElementById('holder');
       holder.ondragover = function () {
